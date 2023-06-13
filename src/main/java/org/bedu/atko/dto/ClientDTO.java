1db99cb4fe88e794c0b4c5ff6c4489af94a00c1a
AlmaLst <alma.loverasotelo@gmail.com>
2023-06-13 04:01:21 -0600

    DTO first version

--- src/main/java/org/bedu/atko/dto/ClientDTO.java
+++ src/main/java/org/bedu/atko/dto/ClientDTO.java
@@ -1,2 +1,13 @@
-package org.bedu.atko.dto;public class ClientDTO {
-}
+//package org.bedu.atko.dto;
+//
+//import lombok.Data;
+//
+//@Data
+//public class ClientDTO {
+//
+//    private long id;
+//    private String name;
+//    private int edad;
+//    private String telefono;
+//    private String email;
+//}
--- src/main/java/org/bedu/atko/dto/ProfessionalDTO.java
+++ src/main/java/org/bedu/atko/dto/ProfessionalDTO.java
@@ -1,2 +1,14 @@
-package org.bedu.atko.dto;public class ProfessionalDTO {
-}
+//package org.bedu.atko.dto;
+
+//import lombok.Data;
+//
+//@Data
+//public class ProfessionalDTO {
+//    private long id;
+//    private String name;
+//    private int edad;
+//    private String telefono;
+//    private String email;
+//    private String areaTrabajo;
+//    private String categoria;
+//}
