cb1d12c6c98d89ec33d5f8f3207500b638bb2a92
AlmaLst <alma.loverasotelo@gmail.com>
2023-06-14 14:44:46 -0600

    Update professional entity/controllers

--- src/main/java/org/bedu/atko/repository/IProfessionalRepository.java
+++ src/main/java/org/bedu/atko/repository/IProfessionalRepository.java
@@ -1,6 +1,6 @@
 package org.bedu.atko.repository;
 
-import org.bedu.atko.model.Professional;
+import org.bedu.atko.entity.Professional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
